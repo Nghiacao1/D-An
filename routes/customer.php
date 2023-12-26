b2590c1b5151267d428d5662cc7e1699e014ed2e
Nghiacao1 <nghiacao.080602@gmail.com>
2023-12-26 17:19:06 +0700

    login

--- routes/customer.php
+++ routes/customer.php
@@ -1,6 +1,7 @@
 <?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomersController;
 
-Route::get('customer-login', [CustomersController::class,'customerLogin'])->name('CustomerSignin');
-Route::post('customer-login', [CustomersController::class,'customerLoginPost'])->name('customer-login');
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerLoginController;
+
+Route::get('customer-login', [CustomerLoginController::class,'customerLogin'])->name('CustomerSignin');
+Route::post('customer-login', [CustomerLoginController::class,'customerLoginPost'])->name('customer-login');
