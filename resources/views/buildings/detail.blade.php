e9740776c5bf8457b442d0a3e5292d220c29b544
DiepAnhDinh <parkrenrenmoe@gmail.com>
2024-04-25 09:56:47 +0700

    ehe

--- resources/views/buildings/detail.blade.php
+++ resources/views/buildings/detail.blade.php
@@ -3,34 +3,55 @@
 @section('content')
 <div class="container">
     <div class="card">
-        <div class="card-header"><h2> Detail building: {{$buildings->name}} </h2></div>
-        <div class="panel-body-1">
-            <div id="buildings-table_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control-2 form-control-sm" placeholder="" aria-controls="buildings-table"></label></div>
+        <div class="card-header"><h2> Detail Building: {{$buildings->fullname}} </h2></div>
+        <div class="panel-body">
+            <div id="buildings-table_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="buildings-table"></label></div>
             <p class="d-flex flex-row-reverse"><a class="btn btn-primary" href="/buildings">Về danh sách</a></p>
                 <div class="box_oldproduct box_oldproductV2"  style="display: flex;margin-top: 20px">   
                     
-                    <img width="150" height="150" alt="hinhanh" src="{{ Avatar::create($buildings->name)->toBase64() }}">
+                    <img width="150" height="150" alt="hinhanh" src="{{ Avatar::create($buildings->fullname)->toBase64() }}">
                     
                     <ul class="col-sm-22">
                         <div class="container-1">
                             <div class="row">
-                              <label class="col">id:</label>
-                              <div class="col">{{$buildings->id}}</div>
+                              <label class="col">User Name:</label>
+                              <div class="col">{{$buildings->username}}</div>
                               <div class="w-100"></div>
-                              <label class="col">name:</label>
-                              <div class="col">{{$buildings->name}}</div>
+                              <label class="col">Neolock Username:</label>
+                              <div class="col">{{$buildings->neolock_username}}</div>
                               <div class="w-100"></div>
-                              <label class="col">hotelId:</label>
-                              <div class="col">{{$buildings->hotelId}}</div>
+                              <label class="col">Email:</label>
+                              <div class="col">{{$buildings->email}}</div>
                               <div class="w-100"></div>
-                              <label class="col">code:</label>
-                              <div class="col">{{$buildings->code}}</div>
+                              <label class="col">Phone Number:</label>
+                              <div class="col">{{$buildings->phone}}</div>
+                              <div class="w-100"></div>
+                              <label class="col">CCCD:</label>
+                              <div class="col">{{$buildings->CCCD}}</div>
                               <div class="w-100"></div>
                               <label class="col"></label>
                               <div class="col"></div
                           </div>
                         </div>
                     </ul>
+                    <ul class="col-sm-22">
+                        <div class="container-1">
+                            <div class="row">
+                              <label class="col">Address:</label>
+                              <div class="col">{{$customers->address}}</div>
+                              <div class="w-100"></div>
+                              <label class="col">Country:</label>
+                              <div class="col">{{$customers->country}}</div>
+                              <div class="w-100"></div>
+                              <label class="col">City:</label>
+                              <div class="col">{{$customers->city}}</div>
+                              <div class="w-100"></div>
+                              <label class="col">District:</label>
+                              <div class="col">{{$customers->district}}</div>
+                          </div>
+                        </div>
+                    </ul>
+
                 </div>
 
 @endsection
