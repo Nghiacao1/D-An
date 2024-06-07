e1b6f4505cd6afc12e23618a833bef219456664e
DiepAnhDinh <parkrenrenmoe@gmail.com>
2024-06-07 19:19:59 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {   
         $productss = DB::table('products')->paginate(8);
-        $productsShow = DB::table('products')->paginate(12);
+        $productsShow = DB::table('products')->paginate(20);
         $cuscount = Customer::count();
         $ordercount = Orderdetail::count();
         $builcount = Building::count();
